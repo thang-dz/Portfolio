b20a3916d492befe028d8228ec88ab9f8e1f7a6f
thang-dz <vumanhthang711@gmail.com>
2026-02-28 21:53:53 +0700

    code

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import tailwindcss from "@tailwindcss/postcss";
 
 export default defineConfig({
   plugins: [pluginReact()],
-  tools: {
-    postcss: (opt, { addPlugins }) => {
-      addPlugins(tailwindcss());
-    },
-  },
 });
